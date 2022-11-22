e0f8175c7949cda594a96833c950905ce761290d
Artem <artem.nodusty@gmail.com>
2022-11-22 23:27:59 +0300

    Added programm

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿string[] inputArray = new string[11] { "hello", "2", "world", ":-)", "1234", "1567", "-2", "computer science", "Russia", "Denmark", "Kazan" };
+int lengthLimit = 3;
+int resultItems = CheckArray(inputArray, lengthLimit);
+string[] outputArray = new string[resultItems];
+void WriteNewArray(string[] oldArray, string[] newArray, int lengthLimit)
+{
+    int temp = 0;
+    for (int i = 0; i < oldArray.Length; i++)
+    {
+        if (oldArray[i].Length <= lengthLimit)
+        {
+            newArray[temp] = oldArray[i];
+            temp++;
+        }
+    }
+}
+int CheckArray(string[] array, int lengthLimit)
+{
+    int result = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= lengthLimit)
+            result++;
+    }
+    return result;
+}
+string PrintArray(string[] array)
+{
+    string result = string.Empty;
+    for (int i = 0; i < array.Length; i++)
+    {
+        result += $"{array[i]}";
+        if (i < array.Length - 1)
+            result += ", ";
+    }
+    return result;
+}
+WriteNewArray(inputArray, outputArray, lengthLimit);
+Console.WriteLine($"{PrintArray(outputArray)}");
